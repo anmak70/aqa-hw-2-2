2e7371ff63e26b32fb1f03c63c12472767e4cf03
Andrey Makolov <anmak70@mail.ru>
2019-12-22 19:05:14 +0300

    Correction tests

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,9 +1,9 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-//import java.time.LocalDate;
-//import java.time.format.DateTimeFormatter;
-//import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -11,8 +11,12 @@ public class TestCardDelivery {
 
     @Test
     void shouldScheduledMeeting() {
+        LocalDate date = LocalDate.now().plusDays(4);
+        String datePlus = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         open("http://localhost:9999");
         $("[data-test-id = city] input").setValue("Белгород");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[data-test-id=date] input").setValue(datePlus);
         $("[data-test-id = name] input").setValue("Иванов-Петров Иван");
         $("[data-test-id = phone] input").setValue("+79204444444");
         $("[data-test-id = agreement]").click();
@@ -21,34 +25,18 @@ public class TestCardDelivery {
     }
 
     @Test
-    void shouldScheduledMeetingCity() {
+    void shouldScheduledMeetingCityDate() {
+        LocalDate date = LocalDate.now().plusDays(7);
+        String datePlus = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         open("http://localhost:9999");
         $("[data-test-id = city] input").setValue("Бе");
-        //Thread.sleep(5000);
-        $$(".menu-item__control").first().click();
+        $$(".menu-item__control").find(Condition.text("Белгород")).click();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[data-test-id=date] input").setValue(datePlus);
         $("[data-test-id = name] input").setValue("Иванов-Петров Иван");
         $("[data-test-id = phone] input").setValue("+79204444444");
         $("[data-test-id = agreement]").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
         $("[data-test-id = notification]").waitUntil(Condition.visible, 15000);
     }
-
-//    @Test
-//    void shouldScheduledMeetingDate() throws InterruptedException {
-//        LocalDate date = LocalDate.now().plusDays(7);
-//        String date7 = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-//        open("http://localhost:9999");
-//        $("[data-test-id = city] input").setValue("Бе");
-//        $$(".menu-item__control").first().click();
-//        $("[data-test-id = date] .icon_name_calendar").click();
-//        Thread.sleep(5000);
-//        $("[data-test-id = date] [class = input__control]").setValue(date7);
-//        Thread.sleep(5000);
-//        $("[data-test-id = name] input").setValue("Иванов-Петров Иван");
-//        $("[data-test-id = phone] input").setValue("+79204444444");
-//        $("[data-test-id = agreement]").click();
-//        $$("button").find(Condition.exactText("Забронировать")).click();
-//        $("[data-test-id = notification]").waitUntil(Condition.visible, 15000);
-//    }
-
 }
